6f65832788bf4706ca097b4ed47413c080d5b0be
hassankh203 <hassankh203@gmail.com>
2022-12-09 21:11:27 -0500

    for jenkins

---
+++ src/jenkinsproject/Jenkinsproject.java
@@ -0,0 +1,17 @@
+package jenkinsproject;
+
+public class Jenkinsproject {
+
+	public static void main(String[] args) {
+		String[] cars = {"volvo","nissan","honda"};
+		for (int i = 0;i<cars.length; i++) {
+			
+		
+		System.out.println(cars[i]);}
+		
+		
+		
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hassa
+ *
+ */
+module jenkinsproject {
+}
